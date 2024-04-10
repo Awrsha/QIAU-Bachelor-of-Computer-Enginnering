5dd6ff9d80e0808f07d2df8cf32d2ba20283a168
Amir M. Parvizi <official.parvizi@gmail.com>
2024-04-10 19:48:33 +0430

    Update Logical Circuit Laboratory

--- Logical Circuit Laboratory/Basic-Half-Adder.v
+++ Logical Circuit Laboratory/Basic-Half-Adder.v
@@ -1,6 +1,11 @@
-module half_adder(a,b,carry,sum);
-	input a,b;
-	output carry,sum;
-	xor(sum,a,b);
-	and(carry,a,b);
+module half_adder(a, b, carry, sum);
+    input a, b;
+    output carry, sum;
+    wire w1, w2;
+    
+    // XOR gate to generate sum
+    xor(sum, a, b);
+    
+    // AND gate to generate carry
+    and(carry, a, b);
 endmodule
--- Logical Circuit Laboratory/Buffer.v
+++ Logical Circuit Laboratory/Buffer.v
@@ -1,24 +1,32 @@
-module buff(x,f,cout);
-  input [7:0] x;
-  output [7:0] f;
-  output cout;
-  buf(f[0],0);
-  buf(f[1],x[0]);
-  buf(f[2],x[1]);
-  buf(f[3],x[2]);
-  buf(f[4],x[3]);
-  buf(f[5],x[4]);
-  buf(f[6],x[5]);
-  buf(f[7],x[6]);
-  buf(cout,x[7]);
+module buff(x, f, cout);
+    input [7:0] x;
+    output [7:0] f;
+    output cout;
+
+    // Instantiate 8 buffers to copy input to output
+    buf buf0(f[0], 0);
+    buf buf1(f[1], x[0]);
+    buf buf2(f[2], x[1]);
+    buf buf3(f[3], x[2]);
+    buf buf4(f[4], x[3]);
+    buf buf5(f[5], x[4]);
+    buf buf6(f[6], x[5]);
+    buf buf7(f[7], x[6]);
+    
+    // Connect MSB of input directly to cout
+    buf buf_cout(cout, x[7]);
 endmodule
 
 module test_buff();
     reg [7:0] _x;
-    wite [7:0] _f;
-    wire [7:0] _cout;
-    buff m1(.x(_x),.f(_f),.cout(_cout));
+    wire [7:0] _f;
+    wire _cout;
+    
+    // Instantiate buffer module for testing
+    buff m1(.x(_x), .f(_f), .cout(_cout));
+    
+    // Initialize input
     initial begin
-      _x=8'b00000001;
+        _x = 8'b00000001;
     end
 endmodule
