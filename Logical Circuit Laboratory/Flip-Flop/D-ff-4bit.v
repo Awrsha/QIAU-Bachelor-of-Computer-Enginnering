d670d153564cd43efe68b7ae1299cfc5f109832c
Ali Hamidi <140819925+ali-hamidi2000@users.noreply.github.com>
2024-04-14 23:11:44 +0330

    Update D-ff-4bit.v

--- Logical Circuit Laboratory/Flip-Flop/D-ff-4bit.v
+++ Logical Circuit Laboratory/Flip-Flop/D-ff-4bit.v
@@ -1,11 +1,16 @@
-module DFF_4bit (D, clk, rst, Q);
-  input [3:0] D;   // 4-bit data input
-  input clk, rst;
-  output reg [3:0] Q;   // 4-bit output
+module DFF_4bit (
+  input [3:0] D,    // 4-bit data input
+  input clk,        // Clock input
+  input rst,        // Reset input
+  output reg [3:0] Q // 4-bit output
+);
 
-  always @(posedge clk or posedge rst)
-    if (rst == 1)
-      Q <= 4'b0000;
-    else
-      Q <= D;
+// Sequential logic for D flip-flop
+always @(posedge clk or posedge rst) begin
+  if (rst)            // Reset condition
+    Q <= 4'b0000;     // Reset output to 0000
+  else
+    Q <= D;           // Update output with input D on clock edge
+end
+
 endmodule
